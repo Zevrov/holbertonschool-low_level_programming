7295be3d9ab60d6655a1616058fe35ff735f2f67
Zevrov <719@holbertonschool.com>
2019-02-06 20:52:39 +0000

    found out there was a typo in the question

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - prints
+* main - prints and "that piece of art is useful\""
 *
 * Return: returns 1
 */
@@ -10,7 +10,7 @@ int main(void)
 {
 	int Z;
 
-	Z = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", Z);
-	return (1);
+	Z = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", Z);
+		return (1);
 }

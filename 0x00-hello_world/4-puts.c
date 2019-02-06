35de05577dd7fee8f43f8e8510da5f60652941b0
Zevrov <719@holbertonschool.com>
2019-02-06 17:43:44 +0000

    added functionality to 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char str1[15];
+
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	
+	puts(str1);
+
+	return(0);
+}

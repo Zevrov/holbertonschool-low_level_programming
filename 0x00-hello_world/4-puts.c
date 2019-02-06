c32254cfc8e6784caf652a86b7ff29370ccb9b33
Zevrov <719@holbertonschool.com>
2019-02-06 18:28:17 +0000

    used betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
-int main (void)
+/**
+*main - Prints ""Programming is like building a multilingual puzzle" 
+*/
+int main(void)
 {
 	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);

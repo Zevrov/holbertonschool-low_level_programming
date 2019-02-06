42bd901ce48ec6cb9a2ab23f4fadb656a622d836
Zevrov <719@holbertonschool.com>
2019-02-06 19:23:23 +0000

    added functionality to 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+* main - gives ./size64 and ./size32 more detail
+*
+* Return: returns 0
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %dbyte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+	return (0);
+}

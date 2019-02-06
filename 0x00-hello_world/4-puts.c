e58e725d2582a19070339ad7cc36f2f724a908fc
Zevrov <719@holbertonschool.com>
2019-02-06 18:33:14 +0000

    maybe I got the formatting right this time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
-*main - Prints ""Programming is like building a multilingual puzzle" 
+*main - Prints ""Programming is like building a multilingual puzzle"
+*
+*Return: returns 0
 */
 int main(void)
 {

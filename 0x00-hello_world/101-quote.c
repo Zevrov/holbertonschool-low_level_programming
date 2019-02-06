ee4c6cef4ff3604c5303503342fc2b88c3413a29
Zevrov <719@holbertonschool.com>
2019-02-06 19:51:19 +0000

    found out how to do 100 from stack exchange, found out how to do 101 from http://codewiki.wikidot.com/c:system-calls:write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - prints
+*
+* Return: returns 1
+*/
+
+int main(void)
+{
+	int Z;
+
+	Z = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", Z);
+	return (1);
+}

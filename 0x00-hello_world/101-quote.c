e913cbdb9ad46f616596f52be7538bd00d6f886f
Zevrov <719@holbertonschool.com>
2019-02-06 21:16:54 +0000

    finally read up on standard output and input, realized you can have 2 error messages that together look like standard output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - prints and "that piece of art is useful\""
+* main - prints the standard error sandwiched around the standard output
 *
 * Return: returns 1
 */
 
 int main(void)
 {
-	int Z;
+	int A;
+	int B;
+	int C;
 
-	Z = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", Z);
-		return (1);
+	A = sizeof("and that piece of art is useful");
+	B = sizeof("\"");
+	C = sizeof(" - Dora Korpar, 2015-10-19\n");
+
+	write(2, "and that piece of art is useful", A);
+	write(1, "\"", B);
+	write(2, " - Dora Korpar, 2015-10-19\n", C);
+	return (1);
 }

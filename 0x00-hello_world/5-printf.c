df64895dc747c7bcb5b093f9729fb7bb64325613
Zevrov <719@holbertonschool.com>
2019-02-06 18:55:09 +0000

    finally fixed all formatting errors

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main (void)
+/**
+* main - prints "with proper grammar, but the outcome is a piece of art,"
+*
+* Return: returns 0
+*/
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

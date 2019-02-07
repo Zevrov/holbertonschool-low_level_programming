f328965d50f08f8a753a354a27544cb081f86dfe
Zevrov <719@holbertonschool.com>
2019-02-07 18:00:05 +0000

    used && for the double condition on the last if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	if (last < 6, last != 0)
+	if (last < 6 && last != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}

4e0e28ae9e9e413a08223d6f92be3069f1630762
Zevrov <719@holbertonschool.com>
2019-02-07 17:53:56 +0000

    tried to fix operation syntax again

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	if (n < 6 != 0)
+	if (n < 6, n != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}

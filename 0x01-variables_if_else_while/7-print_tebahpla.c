c042df94cfbd1b3866c4b9c646275ab7eb14d017
Zevrov <719@holbertonschool.com>
2019-02-07 19:30:38 +0000

    added functionality to 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - prints the lower case alphabet backwards
+* Return: returns 0
+*/
+int main(void)
+{
+	int Z;
+
+	for (Z = 'z'; Z >= 'a'; Z--)
+	{
+	putchar(Z);
+	}
+	putchar('\n');
+	return (0);
+}

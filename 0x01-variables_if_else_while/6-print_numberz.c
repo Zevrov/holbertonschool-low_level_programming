7e11b43e2d47412bd530f2d8af4548b4b1056ee3
Zevrov <719@holbertonschool.com>
2019-02-07 19:26:57 +0000

    added functionality to 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - prints all single digits in base 10
+* Return: returns 0
+*/
+int main(void)
+{
+	int Z;
+
+	for (Z = '0'; Z <= '9'; Z++)
+	{
+	putchar(Z);
+	}
+	putchar('\n');
+	return (0);
+}

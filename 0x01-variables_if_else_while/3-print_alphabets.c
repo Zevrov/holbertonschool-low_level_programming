18c2be856f3d84c6b9ffdd648a4ece635d3dcb3f
Zevrov <719@holbertonschool.com>
2019-02-07 19:12:50 +0000

    added functionality to 4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - prints the lowercase english alphabet but not e or q
+* Return: returns 0
+*/
+int main(void)
+{
+	char Z;
+
+	for (Z != 'e' Z != 'q'; Z = 'a'; Z <= 'z'; Z++)
+	{
+	putchar (Z);
+	}
+	putchar ('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - prints the lowercase english alphabet but not e or q
+* Return: returns 0
+*/
+int main(void)
+{
+	char Z;
+
+	for (Z = 'a' ; Z <= 'z' ; Z++)
+	{
+	if (Z != 'q' && Z != 'e')
+		{
+		putchar (Z);
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+}

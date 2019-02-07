c9c9254b00b735f46e656d6487564fc08b29188b
Zevrov <719@holbertonschool.com>
2019-02-07 06:21:11 +0000

    added functionality to 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,29 @@
-#!/bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - prints if n is positive or negative or 0
+* return: returns 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	if (n < 0)
+	{
+	printf("%d is negative\n", n);
+	}
+	if (n == 0)
+	{
+	printf("%d is 0", n);
+	}
+	
+	return (0);
+}

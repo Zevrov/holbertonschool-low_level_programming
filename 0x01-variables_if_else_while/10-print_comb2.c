0f9f6c2072cb17d77a5e767f8091ea9646ef836f
Zevrov <719@holbertonschool.com>
2019-02-07 20:50:46 +0000

    added functionality to 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,1 +1,26 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - prints out numbers 0-99 in decimal
+* Return: returns 0
+*/
+int main(void)
+{
+	int X;
+	int Z;
+
+	for (X = '0'; X <= '9'; X++)
+	{
+		for (Z = '0'; Z <= '9'; Z++)
+		{
+			putchar(X);
+			putchar(Z);
+			if (X < '9' || Z < '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

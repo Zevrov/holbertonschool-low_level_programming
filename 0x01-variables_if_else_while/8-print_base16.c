4c3dd817d8472326f830a6614f8372714e565438
Zevrov <719@holbertonschool.com>
2019-02-07 19:36:40 +0000

    added functionality to 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - prints the singular digits of hexadecimal
+* Return: returns 0
+*/
+int main(void)
+{
+	int Z;
+	char z;
+
+	for (Z = '0' ; Z <= '9' ; Z++)
+	{
+	putchar(Z);
+	}
+	for (z = 'a' ; z <= 'f' ; z++)
+	{
+	putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}

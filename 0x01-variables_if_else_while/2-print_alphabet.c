590894922fd0027418ac1fe41aaa39cf31df736c
Zevrov <719@holbertonschool.com>
2019-02-07 18:29:12 +0000

    added funcitonality to 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - will print the lower case letters of the english alphabet
+* Return: returns 0
+*/
+
+int main(void)
+{
+	char Z;
+
+	for (Z = 'a' ; Z <= 'z' ; Z++)
+	{
+	putchar (Z);
+	}
+	putchar ('\n');
+
+	return (0);
+}

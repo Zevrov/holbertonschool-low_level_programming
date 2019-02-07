ca042516c693dd7f6c4f6d6167b857f58eb9bbf8
Zevrov <719@holbertonschool.com>
2019-02-07 20:11:55 +0000

    added functionality to 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - prints all possible combinations of single digit numbers
+* Return: returns 0
+*/
+int main(void)
+{
+	int Z;
+
+	for (Z = '0'; Z <= '9'; Z++)
+	{
+	putchar(Z);
+		if (Z < '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

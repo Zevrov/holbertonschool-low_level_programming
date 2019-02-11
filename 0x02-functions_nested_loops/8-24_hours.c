2d3755e475fe30cf1aa741e509e26f59cde41e77
Zevrov <719@holbertonschool.com>
2019-02-11 23:06:22 +0000

    lost 4 hours because I didn't know how to break a for loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,29 +11,24 @@ void jack_bauer(void)
 
 	for (W = '0'; W <= '2'; W++)
 	{
-		if (W != '2')
+		for (X = '0'; X <= '9'; X++)
 		{
-			for (X = '0'; X <= '9'; X++)
-		}
-		else
-		{
-			for (X = '0'; X <= '4'; X++)
-		}
-			if (X != '4')
-			{
 			for (Y = '0'; Y <= '5'; Y++)
 			{
 				for (Z = '0'; Z <= '9'; Z++)
 				{
-				_putchar(W);
-				_putchar(X);
-				_putchar(':');
-				_putchar(Y);
-				_putchar(Z);
-				_putchar('\n');
+					if (W >= '2' && X >= '4' && Y >= '0' && Z >= '0')
+					break;
+					else
+					{
+						_putchar(W);
+						_putchar(X);
+						_putchar(':');
+						_putchar(Y);
+						_putchar(Z);
+						_putchar('\n');
+					}
 				}
-			else
-			
 			}
 		}
 	}

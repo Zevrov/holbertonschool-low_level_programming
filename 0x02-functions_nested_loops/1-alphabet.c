56376c6103b1e9703b32b92760ef73b6436511ec
Zevrov <719@holbertonschool.com>
2019-02-11 15:59:06 +0000

    found out how to make my own .h file and functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include "holberton.h"
+/**
+* main - print the lowercase alphabet backwards.
+* Return: Always 0.
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void print_alphabet(void)
+{
+	char Z;
+
+	for (Z = 'a'; Z <= 'z'; Z++)
+	{
+		putchar(Z);
+	}
+	putchar('\n');
+}

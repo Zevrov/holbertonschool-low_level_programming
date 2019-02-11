c31b28f8bb6daf7fb741f4888ee5ccd152a0f0af
Zevrov <719@holbertonschool.com>
2019-02-11 15:09:10 +0000

    added functionality to 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - prints Holberton followed by a new line
+* Return: returns 0
+*/
+int main(void)
+{
+	printf("Holberton");
+	putchar('\n');
+	return (0);
+}

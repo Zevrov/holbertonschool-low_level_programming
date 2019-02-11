1d969861ad0edf35d7c3d75330ae4c0cea917fd3
Zevrov <719@holbertonschool.com>
2019-02-11 18:52:20 +0000

    added functionality to 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,26 @@
-#!/bin/bash
+#include "holberton.h"
+/**
+* print_sign - prints if the value is positive, negative
+* or 0.
+* @n: is the number being tested
+* Return: 1 if positive, 0 if 0, -1 if negative.
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -3,3 +3,4 @@ void print_alphabet(void);
 void print_alphabet_x10();
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int c);

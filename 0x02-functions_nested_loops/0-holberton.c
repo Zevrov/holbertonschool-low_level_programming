53fb074b8fb08946bc286026eea9e57731f18ddc
Zevrov <719@holbertonschool.com>
2019-02-11 22:29:54 +0000

    went back and fixed 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
 * main - prints Holberton followed by a new line
 * Return: returns 0
 */
 int main(void)
 {
-	printf("Holberton");
-	putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,40 @@
-#!/bin/bash
+#include "holberton.h"
+/**
+* jack_bauer - runs a 24 hour clock simulation
+*/
+void jack_bauer(void)
+{
+	int W;
+	int X;
+	int Y;
+	int Z;
+
+	for (W = '0'; W <= '2'; W++)
+	{
+		if (W != '2')
+		{
+			for (X = '0'; X <= '9'; X++)
+		}
+		else
+		{
+			for (X = '0'; X <= '4'; X++)
+		}
+			if (X != '4')
+			{
+			for (Y = '0'; Y <= '5'; Y++)
+			{
+				for (Z = '0'; Z <= '9'; Z++)
+				{
+				_putchar(W);
+				_putchar(X);
+				_putchar(':');
+				_putchar(Y);
+				_putchar(Z);
+				_putchar('\n');
+				}
+			else
+			
+			}
+		}
+	}
+}

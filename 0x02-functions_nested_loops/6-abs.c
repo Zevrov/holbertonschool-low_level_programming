7468037a4feebf8a9eaa0df77ffd40604468183f
Zevrov <719@holbertonschool.com>
2019-02-11 20:10:31 +0000

    added more functionality

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include "holberton.h"
+/**
+* _abs - returns the absolute value of an integer
+* @int: the integer you want the absolute value of
+* Return: Returns 0
+*/
+int _abs(int n)
+{
+	if (n => 0)
+	{
+		return (n);
+	}
+	if (n < 0)
+	{
+		return ((n * -1));
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include "holberton.h"
+/**
+* print_last_digit - prints the last digit of a number
+* @n: the number that is being used to print
+* Return: returns n after the last digit remains
+*/
+int print_last_digit(int n)
+{
+	_abs(n);
+	if (n >= 10)
+	{
+		n % 10;
+	}
+	_putchar(n);
+	return (n);
+}

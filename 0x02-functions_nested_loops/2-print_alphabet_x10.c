4a8f16fabde080df1337c50a91b85470b25f3176
Zevrov <719@holbertonschool.com>
2019-02-12 09:46:25 +0000

    fixed type conflict

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 /**
 * print_alphabet_x10 - print the lower case alphabet 10 times
-* Return: Always 0.
 */
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	int i;
 

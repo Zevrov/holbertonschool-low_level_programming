3024e4d16530dcbb289a691eeeef6a8b6e91a95e
Zevrov <719@holbertonschool.com>
2019-02-12 01:07:06 +0000

    10 is working, 9 and 11 are work in progress

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include "holberton.h"
+/**
+* add - adds together two integers a + b
+* @a: the first integer
+* @b: the second integer
+* Return: returns the sum of a and b.
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = (a + b);
+	return (c);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,29 @@
-#!/bin/bash
+#include "holberton.h"
+/**
+* times_table - prints the times table on a 9x9 grid
+*/
+void times_table(void)
+{
+	int X;
+	int Y;
+	int Z;
+
+	for (X = 0; X <= 9; X++)
+	{
+		for (Y = 0; Y <= 9; Y++)
+		{
+			Z = (X * Y);
+			_putchar('0' + Z);
+			if (Y <= 8)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			if (Y == 9)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}

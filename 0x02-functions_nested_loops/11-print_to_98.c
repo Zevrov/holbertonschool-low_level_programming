154871ae1a846563906e8fc8065ae773c86880f1
Zevrov <719@holbertonschool.com>
2019-02-12 10:55:47 +0000

    fixed formatting

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,14 @@ void print_to_98(int n)
 	{
 		for (n = n; n > 98; n--)
 		{
-			printf("%d ,", n);
+			printf("%d, ", n);
 		}
 	}
 	if (n < 98)
 	{
 		for (n = n; n < 98; n++)
 		{
-			printf("%d ,", n);
+			printf("%d, ", n);
 		}
 	}
 	if (n == 98)
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,6 +14,6 @@ int print_last_digit(int n)
 	{
 		n = n % 10;
 	}
-	_putchar(n + '0');
+	_putchar('0' + n);
 	return (n);
 }

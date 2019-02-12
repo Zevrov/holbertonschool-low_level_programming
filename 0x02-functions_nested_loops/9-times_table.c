879a43c661cfffbbeb3c56187ad7da2bd2f81859
Zevrov <719@holbertonschool.com>
2019-02-12 01:34:36 +0000

    finally figured out 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,19 +6,28 @@ void times_table(void)
 {
 	int X;
 	int Y;
-	int Z;
 
 	for (X = 0; X <= 9; X++)
 	{
 		for (Y = 0; Y <= 9; Y++)
 		{
-			Z = (X * Y);
-			_putchar('0' + Z);
-			if (Y <= 8)
+			if (X == 0 && Y == 0)
+			{
+				_putchar('0');
+			}
+			if ((X * Y) > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (((X * Y) % 100) / 10));
+				_putchar('0' + ((X * Y) % 10));
+			}
+			if ((X * Y) <= 9 && (X * Y) > 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar('0' + (X * Y));
 			}
 			if (Y == 9)
 			{

6b13dd646ff6e967fbf23ebe73866ece97e2dc82
Zevrov <719@holbertonschool.com>
2019-02-12 14:24:09 +0000

    apparently had to reorder steps to appease checker

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,15 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		n = n * -1;
+		n = n % -10;
 	}
-	if (n > 9)
+	else
 	{
 		n = n % 10;
+	}
+	if (n < 0)
+	{
+		n = n * -1;
 	}
 	_putchar('0' + n);
 	return (n);

47f343a0744961313af733c93140990c876683a1
Zevrov <719@holbertonschool.com>
2019-02-12 02:16:01 +0000

    just read I was allowed to use standard library for 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,27 @@
-#!/bin/bash
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_to_98 - prints the numbers from n to 98
+* @n: the number you want to print to 98 from
+*/
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (n = n; n > 98; n--)
+		{	
+			printf("%d ," , n);
+		}
+	}
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+		{
+			printf("%d ,", n);
+		}
+	}
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+}

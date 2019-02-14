91dfc7522ec1d7b7159bc006e85bab5e4351a7c4
Zevrov <719@holbertonschool.com>
2019-02-14 20:37:28 +0000

    fixed the last bit of the fizz buzz test

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,11 +9,13 @@ int main(void)
 
 	for (X = 1; X <= 100; X++)
 	{
+		if (X == 100)
+			printf("Buzz");
 		if ((X % 3) == 0 && (X % 5) == 0)
 			printf("FizzBuzz ");
 		if ((X % 3) == 0 && (X % 5) != 0)
 			printf("Fizz ");
-		if ((X % 5) == 0 && (X % 3) != 0)
+		if ((X % 5) == 0 && (X % 3) != 0 && X != 100)
 			printf("Buzz ");
 		if ((X % 5) != 0 && (X % 3) != 0)
 			printf("%d ", X);

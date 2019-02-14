df438d0a988fce5bacf1aa95689d6695db254836
Zevrov <719@holbertonschool.com>
2019-02-14 17:00:10 +0000

    fixed spelling in holberton.h

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -1,7 +1,7 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-int mult(int a, int b);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);

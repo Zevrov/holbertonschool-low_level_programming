0efba43d60a7e4d1bfbfd880cb3a23a8b010fb78
Zevrov <719@holbertonschool.com>
2019-02-14 19:28:00 +0000

    added function to 4

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 /**
  * print_most_numbers - prints numbers 0-9 if they aren't 2 or 4
  */
@@ -5,8 +6,8 @@ void print_most_numbers(void)
 {
 	int Z;
 
-	for (Z = '0'; Z < '9'; Z++)
-		if (Z != 2 or Z != 4)
+	for (Z = '0'; Z <= '9'; Z++)
+		if (Z != '2' && Z != '4')
 			_putchar(Z);
 	_putchar('\n');
 }

239da3bd3a2985ae02298d86780060b3e349ee7c
Zevrov <719@holbertonschool.com>
2019-02-14 19:14:08 +0000

    fixed gcc issue with 3

--- 0x03-more_functions_nested_loops/3-print_numbers.c
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,14 @@
 /**
  * print_numbers - prints digits 0-9
  */
+int _putchar(char c);
 void print_numbers(void)
 {
-	char Z;
+	int Z;
 
-	for (Z = '0'; Z < '9'; Z++)
-		_putchar(Z);
+	for (Z = 0; Z <= 9; Z++)
+	{
+		_putchar('0' + Z);
+	}
 	_putchar('\n');
 }

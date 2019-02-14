75cb4619eec5d6667b321555a41fa9e1146f3e36
Zevrov <719@holbertonschool.com>
2019-02-14 19:35:19 +0000

    added functionality to 5

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 /**
  * more_numbers - prints 10 times the numbers 0-14 followed by a new line
  */
@@ -9,20 +10,17 @@ void more_numbers(void)
 
 	for (Z = 0; Z < 9; Z++)
 	{
-		_putchar('\n');
-		for (Y = '0'; Y < '1'; Y++)
+		for (Y = '0'; Y <= '1'; Y++)
 		{
-			for (X = '0'; X < '9'; X++)
+			for (X = '0'; X <= '9'; X++)
 			{
-				if (Y == '1' && X == '5')
-					break;
 				if (Y == '1')
 					_putchar(Y);
 				_putchar(X);
+				if (Y == '1' && X == '4')
+					break;
 			}
 		}
+		_putchar('\n');
 	}
 }
-
-
-

5e8ec85287cc89f5e56481ac51473d31f35b54cd
Zevrov <719@holbertonschool.com>
2019-02-18 17:38:12 +0000

    got 4 to work, deleted test files

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -3,13 +3,15 @@
 * _puts - prints a string followed by a new line
 * @str: the string being printed
 */
-void _puts(char *str)
-{
-	while (*str != '\0')
-		str++;
-	for (str; *str != '\0'; str--)
+void print_rev(char *s)
+{	
+	while (*s != '\0')
 	{
-		_putchar(*str);
+		s++;
+	}
+	while (*s >= '\0')
+	{
+		_putchar(*s--);
 	}
 	_putchar('\n');
 }

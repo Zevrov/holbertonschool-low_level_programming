4e0ea43a8082319460457ec489bf7278ae5be6d9
Zevrov <719@holbertonschool.com>
2019-02-18 15:27:16 +0000

    cleaned up directory after testing

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
 /**
 * reset_to_98 - resets the vaule to 98
-* @*n: the value being reset
+* @n: the value being reset
 */
 void reset_to_98(int *n)
 {
-  int n = 98;
-  int *n = &n;
+	*n = 98;
 }

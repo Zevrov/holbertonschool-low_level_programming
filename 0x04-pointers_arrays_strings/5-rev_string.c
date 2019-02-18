56896da666a861550e50bcd0f927793f5d3b6b08
Wolf Hopkins <zevrov.719@gmail.com>
2019-02-18 12:58:57 -0500

    Update 5-rev_string.c

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#inclue "holberton.h"
+/**
+* rev_string - reverses a string
+* @s: the string being reversed
+*/
+void rev_string(char *s)
+{
+	int Temp, x, y;
+
+	y = x - 1;
+	x = 0;
+
+	while (x < y)
+	{
+		Temp = *s[x];
+		*s[x] = *s[y];
+		*s[y] = Temp;
+		x++;
+		y++;
+	}
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		str++;
+	}
+	_putchar('\n');
+}

9a1e0d25800b8471f0152eb772023dc372e42a95
Zevrov <719@holbertonschool.com>
2019-02-18 16:10:58 +0000

    got 2 to work

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s ++;
+		len ++;
+	}
+	return len;
+}

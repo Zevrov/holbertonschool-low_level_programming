f6181c5908ac7414573da1926dff21569a48f8d2
Wolf Hopkins <zevrov.719@gmail.com>
2019-02-18 10:37:50 -0500

    Update 1-swap.c

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* swap_int - swaps the values of two integers
+* @a: the first integer
+* @b: the second integer
+*/
+void swap_int(int *a, int *b)
+{
+  *a = *b;
+  *b = *a;
+}

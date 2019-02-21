51680d192e9df523086423e0c6d387df4e331047
Zevrov <719@holbertonschool.com>
2019-02-21 19:47:34 +0000

    got rid of test files, 0 functions properly

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,12 @@ char *_strcat(char *dest, char *src)
 {
 	int x, y;
 
-	for (x = 0; dest[x] != '\0'; x++);
+	for (x = 0; dest[x] != '\0'; x++)
+		;
 	for (y = 0; src[y] != '\0'; y++)
 	{
 		dest[x + y] = src[y];
 	}
 	dest[x + y] = '\0';
-	return dest;
+	return (dest);
 }

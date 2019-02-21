b83f90fffd09241d51b5e639a0923b8d53c5b233
Wolf Hopkins <zevrov.719@gmail.com>
2019-02-21 14:45:01 -0500

    Update 0-strcat.c

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -12,8 +12,8 @@ char *_strcat(char *dest, char *src)
 	for (x = 0; dest[x] != '\0'; x++);
 	for (y = 0; src[y] != '\0'; y++)
 	{
-		dest[x + y] = src[y]
+		dest[x + y] = src[y];
 	}
-	dest[x + y] = '\0'
+	dest[x + y] = '\0';
 	return dest;
 }

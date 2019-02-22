18ead645a1b199eb00e3726cda31d7d261b4abb4
Zevrov <719@holbertonschool.com>
2019-02-22 04:10:26 +0000

    tested 6

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes first words
+ * @Z: the string
+ * Return: the string altered
+ */
+char *cap_string(char *Z)
+{
+	char a[] = "\n\t .,;:-(){}[]/\'""?¿!¡";
+	int x, y;
+
+	for (x = 0; Z[x] != '\0'; x++)
+	{
+		if (x == 0 && Z[x] >= 'a' && Z[x] <= 'z')
+			Z[x] = Z[x] - 32;
+		for (y = 0; a[y] != '\0'; y++)
+		{
+			if (Z[x] == a[y])
+			{
+				x++;
+				if(Z[x] >= 'a' && Z[x] <= 'z')
+					Z[x] = Z[x] - 32;
+				else
+				{
+					x++;
+					if(Z[x] >= 'a' && Z[x] <= 'z')
+						Z[x] = Z[x] - 32;
+				}
+			}
+		}
+	}
+	return (Z);
+}

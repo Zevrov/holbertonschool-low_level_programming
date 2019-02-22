c982f3ccc6bf67fcbba22bf5b494dba7b3e3f38e
Zevrov <719@holbertonschool.com>
2019-02-22 04:31:43 +0000

    did 7

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,22 @@
-#`
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * @z: the string
+ * Return: The encoded string
+ */
+char *leet(char *z)
+{
+	char l[] = "aAeEoOtTlL";
+	char num[] = "43071";
+	int x, y;
+
+	for (x = 0; z[x] != '\0'; x++)
+	{
+		for (y = 0; l[y] != '\0'; y++)
+		{
+			if (z[x] == l[y])
+				z[x] = num[y / 2];
+		}
+	}
+	return (z);
+}

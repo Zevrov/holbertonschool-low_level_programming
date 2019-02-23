d329a902880155f9c1e296c703b1d68c190b2d32
Zevrov <719@holbertonschool.com>
2019-02-23 19:03:03 +0000

    progress

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -7,17 +7,16 @@
 char *rot13(char *z)
 {
 	int x, y;
-	char a[] = "abcdefghijklm";
-	char A[] = "ABCDEFGHIJKLM";
+	char a[] = "abcdefghijklmABCDEFGHIJKLM";
+	char b[] = "nopqrstuvwxyzNOPQRSTUVWXYZ";
 
 	for (x = 0; z[x] != '\0'; x++)
 	{
+		z[x] = z[x] - 13;
 		for (y = 0; a[y] != '\0'; y++)
 		{
-			if (z[x] == a[y] || z[x] == A[y])
-				z[x] = z[x] + 13;
-			else
-				z[x] = z[x] - 13;
+			if (z[x] + 13 == a[y])
+				z[x] = b[y];
 		}
 	}
 	return (z);

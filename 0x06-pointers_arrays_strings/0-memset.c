857c587a5b2de6ee9c0c13995a2d57c42738d0b8
Zevrov <719@holbertonschool.com>
2019-02-25 17:26:14 +0000

    fixed betty issues

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  * @s: the area being looked at
  * @b: the byte filling memory
  * @n: the amount of memory being filled
+ * Return: the new memory
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -13,5 +14,5 @@ char *_memset(char *s, char b, unsigned int n)
 		s++;
 		n--;
 	}
-	return(s);
+	return (s);
 }

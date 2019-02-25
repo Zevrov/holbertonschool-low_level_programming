c75a18d75f360718a93e5222b4f1e95e9835d4a6
Zevrov <719@holbertonschool.com>
2019-02-25 23:14:20 +0000

    found a way easier way to do it

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix of integers
+ * @a: the diagonal
+ * @size: the size of the diagonals
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, x = 0, y = 0;
+
+	for (i = 0; i < size * size; i += (size + 1))
+		x += a[i];
+	for (i = size - 1; i < (size * size) - (size - 1); i += (size - 1))
+		y += a[i];
+	printf ("%d, %d\n", x, y);
+}
---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *
+ *
+ */
+void set_string(char **s, char *to)
+{
+	char x;
+	x = **s;
+	to = &s;
+}

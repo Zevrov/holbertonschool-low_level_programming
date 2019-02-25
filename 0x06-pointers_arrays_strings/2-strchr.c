caee63f0bf0a9f9dd5e161232746e27447d1871b
Zevrov <719@holbertonschool.com>
2019-02-25 18:10:04 +0000

    testing 2

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ * Return: Null if not found, otherwise s after the character is located
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; ++s)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return ('\0');
+}

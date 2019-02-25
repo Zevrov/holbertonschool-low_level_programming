5d806bec94c16737ca836a7c3633afeda5ea996e
Zevrov <719@holbertonschool.com>
2019-02-25 20:56:46 +0000

    fixed formatting

--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -7,12 +7,11 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; a[i] != a[8]; i++)
+	for (i = 0; i != 8; i++)
 	{
-		for (j = 0; a[i][j] != a[i][8];)
+		for (j = 0; j != 8; j++)
 		{
 			_putchar(a[i][j]);
-			j++;
 		}
 		_putchar('\n');
 	}

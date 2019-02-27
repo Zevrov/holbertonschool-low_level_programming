937629ce8fe6c5aeacc45c1d8312dc6249981db4
Zevrov <719@holbertonschool.com>
2019-02-27 21:56:28 +0000

    test 5

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,17 +1,30 @@
 #include "holberton.h"
 /**
- *
- *
+ * _sqrt - finds the square root of a number
+ * @n: the number
+ * @z: the test for a square
+ * Return: the square root or -1
  */
-int _sqrt_recursion(int n, int z)
+int _sqrt(int n, int z)
 {
 	if (n == 0 || n == 1)
 		return (n);
 	if (n < 0)
 		return (-1);
-	if ((z * z) < n)
-		return (_sqrt_recursion(n ,z + 1));
-	if ((z * z) == n)
+	if ((z * z) > n)
+		return (-1);
+	if ((z*z) == n)
 		return (z);
+	if ((z * z) < n)
+		return (_sqrt(n, z + 1));
+	return (0);
 }
-	
+/**
+ * _sqrt_recursion - finds the square root of a number
+ * @n: the number
+ * Return: The Square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+		return (_sqrt(n, 1));
+}

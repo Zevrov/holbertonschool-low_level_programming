793fb4ef43c3d20776dedb45c6c2642f4ef4a4f8
Zevrov <719@holbertonschool.com>
2019-02-27 18:06:55 +0000

    maybe this fixes the issue?

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -6,13 +6,11 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		return;
-	if (*s != '\0')
 	{
-		_putchar(*s);
-		s++;
-		_puts_recursion(s);
-	}
-	if (*s == '\0')
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }

f7494208be1bfa328c2683e7632ba11c1e9dd4df
Zevrov <719@holbertonschool.com>
2019-02-27 21:38:09 +0000

    testing 6

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 /**
- *
- *
+ * isPrime - tells you if a number is prime or not
+ * @n: the number
+ * @i: the number testing n
+ * Return: 1 if prime, 0 if not
  */
-int isPrime(int n, int i) 
+int isPrime(int n, int i)
 {
 	if (n <= 2)
 		return (0);
@@ -13,11 +15,15 @@ int isPrime(int n, int i)
 		return (1);
 	return (isPrime(n, i + 1));
 }
-
+/**
+ * is_prime_number - tells you if a number is prime or not
+ * @n: the number
+ * Return: 1 if prime, 0 if not
+ */
 int is_prime_number(int n)
 {
 	if (isPrime(n, 2))
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }

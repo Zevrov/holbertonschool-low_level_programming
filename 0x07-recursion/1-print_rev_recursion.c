7c544138da6bea963f1cd3944e842db5edc87a94
Zevrov <719@holbertonschool.com>
2019-02-27 19:39:26 +0000

    fixed betty error

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
- * _print_rev_recursion - prints a string in reverse 
+ * _print_rev_recursion - prints a string in reverse
  * @s: the string
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)	
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);

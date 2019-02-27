961101be69c50cef521fd1efe8a42d5444eb6674
Zevrov <719@holbertonschool.com>
2019-02-27 20:27:33 +0000

    fixed betty issue

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -9,11 +9,11 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return(-1);
+		return (-1);
 	if (y == 0)
 		return (1);
 	else if (y % 2 == 0)
-		return (_pow_recursion(x, y / 2) * _pow_recursion (x, y / 2));
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 	else
 		return (x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 }

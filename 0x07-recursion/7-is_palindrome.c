e1d3f5512a66d3d5685d5b68576fcb0e102bab51
Zevrov <719@holbertonschool.com>
2019-02-27 22:56:11 +0000

    7 test 1

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,23 +1,52 @@
 #include "holberton.h"
 /**
- *
- *
+ * ispal - checks if the string is a palindrome
+ * @z: the string
+ * @s: the start
+ * @e: the end
+ * Return: 1 if a palindrome, 0 if not
+ */
+int ispal(char *z, int s, int e)
+{
+	if (s == e)
+	{
+		return (0);
+	}
+	if (z[s] == z[e])
+	{
+		return (ispal(z, s + 1, e - 1));
+	}
+	else
+	{
+		return (1);
+	}
+	return (0);
+}
+/**
+ * is_palindrome - checks if the string is a palindrome
+ * @s: the string
+ * Return: 1 for a palindrome 0 if not
  */
 int is_palindrome(char *s)
 {
-	char *p;
+	int n = 0;
+	char *p = s;
 
-	if (*s != '\0')
+	while (*p != '\0')
 	{
-		is_palindrome(s + 1);
-		p = s;
-		p++;
-		len++;
+		s++;
+		n++;
 	}
-	len -= 1;
-	s -= len;
-	if (*p == *s)
+	if (n == 0)
+	{
 		return (1);
+	}
+	if (ispal(s, 0, n - 1))
+	{
+		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }

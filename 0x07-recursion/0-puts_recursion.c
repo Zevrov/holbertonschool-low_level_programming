60825bc7e15fe0adeb0df433033d5ee025248031
Zevrov <719@holbertonschool.com>
2019-02-27 18:03:15 +0000

    isnt printing null now

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -7,9 +7,12 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
 	if (*s == '\0')
 		_putchar('\n');
 }

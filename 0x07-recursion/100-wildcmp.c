21521c7f7e419add62ed3214e690ac840eb033eb
Zevrov <719@holbertonschool.com>
2019-02-27 23:24:01 +0000

    nailed it

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * wildcmp - checks if the strings are the same
+ * @s1: the first string
+ * @s2: the second string
+ * Return: 1 if the same 0 if not
  */
 int wildcmp(char *s1, char *s2)
 {

c051f5b1e7c2876b2db31cfebcdf5a2c7e9c2c4e
Zevrov <719@holbertonschool.com>
2019-03-02 20:18:37 +0000

    test 3

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies 2 numbers
+ * @argc: number of args
+ * @argv: array of args
+ * Return: Awlays 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	if (argc == 3)
+	{
+		a = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", a);
+	}
+	else
+	{
+		printf("error\n");
+	}
+	return (0);
+}

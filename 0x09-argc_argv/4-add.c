0556e8c7997b54b85cef571920704f547aa6c1d1
Zevrov <719@holbertonschool.com>
2019-03-02 21:14:16 +0000

    took a long time because I was lazy and didn't want to make another loop lol

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - adds numbers
  * @argc: the number of args
@@ -10,22 +11,29 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int sum;
+	int x;
 	
+	for (i = 1; i < argc; i++)
+	{
+		for (x = 0; argv[i][x] != '\0'; x++)
+		{
+			if (isalpha(argv[i][x]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				for (i = 1; i < argc; i++)
+				{
+					sum += atoi(argv[i]);
+				}
+				printf("%d\n", sum);
+				return (0);
+			}
+		}
+	}
 	if (argc == 1)
 		printf("0\n");
-	for (i = 1; i < argc; i++)
-		if (atoi(argv[i]) <= '0' || atoi(argv[i]) >= '9')
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			for (i = 1; i < argc; i++)
-			{
-				sum += atoi(argv[i]);
-			}
-			printf("%d\n", sum);
-		}
 	return (0);
 }

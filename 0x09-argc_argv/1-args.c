8995cb321293ad18f12850b77d32528488bd3352
Zevrov <719@holbertonschool.com>
2019-03-02 20:06:51 +0000

    testing 1

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - prints the number of arguments
+ * @argc: the number of arg
+ * @argv: the array of arg
+ * Return: Always 0
  */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int count;
+
+	while (argv[i] != '\0' || i == argc - 1)
+	{
+		count++;
+		i++;
+	}
+	printf("%d\n", count);
+	return (0);
+}

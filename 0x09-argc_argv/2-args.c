9641c5904a022c6316da22fc5124e0ec7fd04126
Zevrov <719@holbertonschool.com>
2019-03-02 20:10:30 +0000

    test 2

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the arguments
+ * @argc: the number of args
+ * @argv: the array of args
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}

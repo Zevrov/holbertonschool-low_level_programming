70f297119900bd8d38b6c9b7b0ff006e9cbe1d30
Zevrov <719@holbertonschool.com>
2019-03-05 23:26:13 +0000

    test 0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -11,11 +11,13 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *str;
-	
+
 	i = 0;
 	if (size == 0)
 		return ('\0');
 	str = malloc((size + 1) * sizeof(char));
+	if (!str)
+		return ('\0');
 	while (i < size)
 	{
 		str[i] = c;

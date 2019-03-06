6e445a839c662534f1cd6e61eb4bcdfaa2c5fd24
Zevrov <719@holbertonschool.com>
2019-03-06 03:05:17 +0000

    test 3

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - allocates mem for a grid
+ * @width: the width
+ * @height: the height
+ * Return: Null or the grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int x, y, z;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (; z < height; z++)
+	{
+		grid[z] = malloc(sizeof(int) * width);
+		if (grid[z] == NULL)
+		{
+			return (NULL);
+		}
+	}
+	for (x = 0; x < height; x++)
+		for (y = 0; y < width; y++)
+			grid[x][y] = 0;
+	return (grid);
+}

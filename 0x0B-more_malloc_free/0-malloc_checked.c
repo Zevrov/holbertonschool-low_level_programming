f366acc88b5be6ac8986d543dd9063061bfbbf99
Zevrov <719@holbertonschool.com>
2019-03-07 18:13:22 +0000

    test 1

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - returns a pointer to allocated mem
+ * @b: the amount of memory
+ * Return: returns new or 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *new;
+	new = malloc(b);
+	if (!new)
+		exit(98);
+	return (new);
+}

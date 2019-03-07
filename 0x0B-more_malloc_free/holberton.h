4461bf5133477c13a0e5e7d91d9a7f22fef77f66
Wolf Hopkins <zevrov.719@gmail.com>
2019-03-07 13:01:55 -0500

    Update holberton.h

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -1,4 +1,9 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+
+#endif /* holberton.h */

4e52e6f2fa6fcba655078f4d01d57d8b2203bb30
Zevrov <719@holbertonschool.com>
2019-03-11 19:03:48 +0000

    test 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,12 +11,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *pupper;
+	char *namecpy;
+	char *ownercpy;
+	int i = 0;
 
+	ownercpy = malloc(sizeof(char *));
+	if (!ownercpy)
+		free(ownercpy);
+	namecpy = malloc(sizeof(char *));
+	if (!namecpy)
+		free(namecpy);
 	pupper = malloc(sizeof(struct dog));
 	if (!pupper)
 		return (NULL);
 	pupper->name = name;
 	pupper->age = age;
 	pupper->owner = owner;
+	if ((ownercpy[i] != '\0') && (owner[i] != '\0'))
+	{
+		while (owner[i] != '\0')
+		{
+			ownercpy[i] = owner[i];
+			i++;
+		}
+		ownercpy[i] = '\0';
+	}
+	i = 0;
+	if ((namecpy[i] != '\0') && (name[i] != '\0'))
+	{
+		while (name[i] != '\0')
+		{
+			namecpy[i] = name[i];
+			i++;
+		}
+		namecpy[i] = '\0';
+	}
 	return (pupper);
 }

68511798f5f3cbc9e5bcf5286c13bb5210fedea1
Zevrov <719@holbertonschool.com>
2019-03-11 17:18:48 +0000

    test 0

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+struct Dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a dog
+ * @name: the name of the dog
+ * @age: the dog's age
+ * @owner: the dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+int _putchar(char c);
+
+#endif /* _DOG_H_ */

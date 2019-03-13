e730c7b72042ccbc930972d31705072f14b31997
Zevrov <719@holbertonschool.com>
2019-03-13 17:45:09 +0000

    test for null 2

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
-	
+
 	if (array != NULL && cmp != NULL)
 	{
 		if (size <= 0)
@@ -24,6 +24,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 			i++;
 		}
 	}
-	return (0);
+	return (-1);
 }
 

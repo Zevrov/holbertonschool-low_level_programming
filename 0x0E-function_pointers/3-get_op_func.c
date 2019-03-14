3da6eef059aab15cbd00e3b62b10dd145f2f24f6
Zevrov <719@holbertonschool.com>
2019-03-14 02:45:35 +0000

    test 3

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -15,9 +15,9 @@ int (*get_op_func(char *s))(int, int)
 	{"%", op_mod},
 	{NULL, NULL}
 	};
-	int i - 0;
+	int i = 0;
 
-	while (ops[i].op != NULL;)
+	while (ops[i].op != NULL)
 	{
 		if (ops[i].op[0] == s[0])
 			return (ops[i].f);

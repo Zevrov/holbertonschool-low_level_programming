5021ea13b80f3c00df06de5a9fc70da19b6f488d
Zevrov <719@holbertonschool.com>
2019-03-14 02:42:28 +0000

    test 2, using a while loop instead of 4

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -15,12 +15,13 @@ int (*get_op_func(char *s))(int, int)
 	{"%", op_mod},
 	{NULL, NULL}
 	};
-	int i;
+	int i - 0;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	while (ops[i].op != NULL;)
 	{
 		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }

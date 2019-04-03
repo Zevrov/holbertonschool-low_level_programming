f7fce6a4d3ffaf0afe96749538430f42789779ca
Zevrov <719@holbertonschool.com>
2019-04-03 03:00:03 +0000

    appeased betty

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 /**
  * read_textfile - reads a text file and prints it to stdout
  * @filename: the file name
- * @letters - what's in the file
+ * @letters: what's in the file
  * Return: the number of letters for reading and printing
  */
 ssize_t read_textfile(const char *filename, size_t letters)
--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -7,6 +7,7 @@
 int _strlen(char *s)
 {
 	int x;
+
 	for (x = 0; s[x] != '\0'; x++)
 		;
 	return (x);

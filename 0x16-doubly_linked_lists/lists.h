8b293705fc662e0b0d811fcd2dfdb2bfd9a371f0
Zevrov <719@holbertonschool.com>
2019-05-05 23:19:58 +0000

    fixed header errer, missing function

--- 0x16-doubly_linked_lists/lists.h
+++ 0x16-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _LISTS_H_
+#define _LISTS_H_
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,6 +23,7 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);

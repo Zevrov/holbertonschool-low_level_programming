7c8db283e82d64f0dffc007d02697ea5df99aaf1
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-05 19:39:51 -0400

    test 5

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -1,10 +1,23 @@
 #include "lists.h"
 /**
-*
-*
-*
+* get_dnodeint_at_index - gives the node at "index"
+* @head: the head of the list
+* @index: where the node you want is
+* Return: returns the node
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *temp;
 
+	if (!head)
+		return (NULL);
+	temp = head;
+	while (index > 0 && temp)
+	{
+		index--;
+		temp = temp->next
+		if (!temp)
+			return (NULL);
+	}
+	return (temp);
 }

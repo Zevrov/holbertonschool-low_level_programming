8ed0d2805c45cb03ec529ff36627f2a84b18e690
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-05 19:41:37 -0400

    forgot a semicolon

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (index > 0 && temp)
 	{
 		index--;
-		temp = temp->next
+		temp = temp->next;
 		if (!temp)
 			return (NULL);
 	}

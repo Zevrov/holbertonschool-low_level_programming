079cbee6dacfef0309d84efea8fc29b004566e3d
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-05 19:47:08 -0400

    test 6

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,18 @@
 #include "lists.h"
 /**
-*
-*
-*
+* sum_dlistint - give the total amount of nodes
+* @head: pointer to the first node
+* Return: the amount of nodes
 */
 int sum_dlistint(dlistint_t *head)
+{
+	int total = 0;
+	dlistint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (total);
+}

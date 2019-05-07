891c2db1236c2b5a298aa486bb5d5075eed0b555
Zevrov <719@holbertonschool.com>
2019-05-07 01:13:13 +0000

    fixing betty errors

--- 0x17-dynamic_libraries/100-atoi.c
+++ 0x17-dynamic_libraries/100-atoi.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
  * _atoi - convert string to numbers
- * @s: the string
+ *
  * Return: n
  */
-int _atoi()
+int _atoi(void)
 {
 	/* int x; */
 	/* char arr[]; */
 
 	/* while (*s) */
 	/* { */
-	/* 	x++; */
-	/* 	if (*s >= 48 && *s <= '9') */
-	/* 		conv[i] = *s */
+	/* x++; */
+	/* if (*s >= 48 && *s <= '9') */
+	/* conv[i] = *s */
 	/* } */
 
 	return (0);

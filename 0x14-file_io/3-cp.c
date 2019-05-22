58a59da52768f94e5e53421796c0d9959a05a150
Zevrov <719@holbertonschool.com>
2019-05-22 21:45:19 +0000

    hopefully this shows I know what I'm doing

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int to_z, from_z, rcount, wcount, close_to_error, close_from_error;
+	int to_z, from_z, rcount, wcount;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -17,19 +17,7 @@ int main(int argc, char *argv[])
 	}
 	to_z = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 00664);
 	from_z = open(argv[1], O_RDONLY);
-	if (to_z == -1 || from_z == -1)
-	{
-		if (to_z == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-		if (from_z == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-	}
+	rw_checker(to_z, from_z, argv[2], argv[1]);
 	rcount = 1024;
 	while (rcount == 1024)
 	{
@@ -46,17 +34,39 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-	close_to_error = close(to_z);
-	if (close_to_error == -1)
+	if (close(to_z) == -1)
 	{
 		dprintf(STDERR_FILENO, "ERROR: Can't close to fd %d\n", to_z);
 		exit(100);
 	}
-	close_from_error = close(from_z);
-	if (close_from_error == -1)
+	if (close(from_z) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_z);
 		exit(100);
 	}
 	return (0);
+}
+/**
+ * rw_checker - makes sure the files are able to be
+ * read and written to/from
+ * @to: the value from the open function of the file being written to
+ * @from: the value from the open function of the file being read from
+ * @to_file: the name of the file being written to
+ * @from_file: the name of the file being read from
+ */
+void rw_checker(int to, int from, char to_file, char from_file)
+{
+	if (to == -1 || from == -1)
+	{
+		if (to == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", to_file);
+			exit(99);
+		}
+		if (from == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't read from %s\n", from_file);
+			exit(98);
+		}
+	}
 }
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -12,5 +12,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void rw_checker(int to, int from, char to_file, char from_file);
 
 #endif

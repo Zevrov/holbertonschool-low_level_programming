4f32b008eca7602cae4c7354d1a47424df09df16
Wolf Hopkins <46612966+Zevrov@users.noreply.github.com>
2019-05-22 17:05:23 -0400

    fixed typo

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -38,7 +38,7 @@ int main(int ac, char *av[])
 	from_z = open(av[1], O_RDONLY);
 	if (from_z == -1)
 	{
-		dprintf(STDERR_FILENO, "Error:, Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	rcount = 1024;

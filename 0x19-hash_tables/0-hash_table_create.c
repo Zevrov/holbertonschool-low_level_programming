8518d9af8811ea520ebd774196057f3a91bf30af
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-31 15:29:35 -0400

    actually malloc'd the correct amount

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
     if (!table)
         return (NULL);
     (*table).size = size;
-    (*table).array = malloc(sizeof(hash_node_t) * size);
+    (*table).array = malloc(sizeof(hash_node_t *) * size);
     if (!(*table).array)
     {
         free(table);

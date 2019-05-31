1a92bab8e3f86ba2b3dcbecef0d1b0b3ce766f4c
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-31 12:39:29 -0400

    testing 1

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* hash_djb2 - hash function djb2 algorithm
+*
+* @str: string input
+* Return: returns hash
+*/
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c;
+
+    return hash;
+}

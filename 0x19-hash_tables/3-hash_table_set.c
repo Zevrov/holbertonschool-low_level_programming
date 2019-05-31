23a5f768e6353d20e16ea68c22cde21e9bff4aef
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-31 16:43:16 -0400

    3 testing

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+* hash_table_set - sets a hash table
+* @ht: the hash table
+* @key: the key
+* @value: the value
+* Return: the index where the key/value is located in the table
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int index;
+
+    index = hash_djb2(key) % size;
+    return (index);
+}

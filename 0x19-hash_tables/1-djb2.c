6bc8729bf955e94b73874addc5969f75fb7ff3b6
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-31 15:32:46 -0400

    updated to work with prototype

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -11,7 +11,8 @@ unsigned long int hash_djb2(const unsigned char *str)
     unsigned long hash = 5381;
     int c;
 
-    while (c = *str++)
+    c = *str;
+    for (;c != '\0'; c++;)
         hash = ((hash << 5) + hash) + c;
 
     return hash;

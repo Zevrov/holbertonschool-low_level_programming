393c404d68fb759084dd04ea32e26cfce33c2426
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-02 14:04:27 -0400

    update to header file

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -1,5 +1,7 @@
 #ifndef _HASH_TABLES_H_
 #define _HASH_TABLES_H_
+
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -32,9 +34,12 @@ typedef struct hash_table_s
     hash_node_t **array;
 } hash_table_t;
 
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
-#endif /* hast_tables.h */
+#endif

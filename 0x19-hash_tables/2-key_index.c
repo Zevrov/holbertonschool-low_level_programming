d40c5048f1a9fc1f7f20f76696d564602fbee3e4
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-02 14:01:42 -0400

    3 was actually meant to be 2

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,10 +1,14 @@
 #include "hash_tables.h"
 /**
-*
-*
-*
+* hash_table_set - sets a hash table
+* @key: the key
+* @size: the value for size
+* Return: where the key/value is stored in the array
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+	return (index);
 }

a7817551d785226af5ca45c93f8432ea282f21c6
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-02 14:38:05 -0400

    change to 3, added unsuccessful check in the event that there is not a new key

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -32,6 +32,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		return (0);
 	}
+	if (!(*new).key)
+	{
+		free(new);
+		return (0);
+	}
 	(*new).value = strdup(value);
 	if (!(*new).value)
 	{

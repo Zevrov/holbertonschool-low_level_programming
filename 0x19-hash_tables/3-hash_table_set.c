99f879a02698e0742bbdf62c5471f1533e9857b4
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-02 14:29:25 -0400

    found typo in 3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp((*temp).key, key) == 0)
 		{
 			free((*temp).value);
-			(*temp).value = strup(value);
+			(*temp).value = strdup(value);
 			return (1);
 		}
 		temp = (*temp).next;

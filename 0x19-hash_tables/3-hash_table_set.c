59c3577ee11a14be92b1bfb2df5e3558a4418ed2
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-02 14:27:11 -0400

    1, problem 3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,15 +1,45 @@
 #include "hash_tables.h"
 /**
-* hash_table_set - sets a hash table
-* @ht: the hash table
+* hash_table_set - adds an element to a hash table
+* @ht: the table you want to add to
 * @key: the key
 * @value: the value
-* Return: the index where the key/value is located in the table
+* Return: 1 on success, 0 otherwise
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+	hash_node_t *new = NULL, *temp = NULL;
 
-	index = hash_djb2(key) % size;
-	return (index);
+	if (strlen(key) == 0 || !key || !ht)
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *)key, (*ht).size);
+	temp = (*ht).array[index];
+	while (temp != NULL)
+	{
+		if (strcmp((*temp).key, key) == 0)
+		{
+			free((*temp).value);
+			(*temp).value = strup(value);
+			return (1);
+		}
+		temp = (*temp).next;
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+	{
+		return (0);
+	}
+	(*new).value = strdup(value);
+	if (!(*new).value)
+	{
+		free((*new).key);
+		free(new);
+		return (0);
+	}
+	(*new).next = (*ht).array[index];
+	(*ht).array[index] = new;
+	return (1);
 }

4fbff410de339fa2cc417d140e5bd56d39e7ed04
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-02 13:24:39 -0400

    fixed betty errors

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -7,19 +7,19 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table = NULL;
+	hash_table_t *table = NULL;
 
-    if (size == 0)
-        return (NULL);
-    table = malloc(sizeof(hash_table_t));
-    if (!table)
-        return (NULL);
-    (*table).size = size;
-    (*table).array = malloc(sizeof(hash_node_t *) * size);
-    if (!(*table).array)
-    {
-        free(table);
-        return (NULL);
-    }
-    return (table);
+	if (size == 0)
+		return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	(*table).size = size;
+	(*table).array = malloc(sizeof(hash_node_t *) * size);
+	if (!(*table).array)
+	{
+		free(table);
+		return (NULL);
+	}
+	return (table);
 }

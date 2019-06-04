43f5ae013abcf1ec8480ba304c8202df5bb36c28
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-03 23:04:10 -0400

    deletion without mem leaks complete, testing with checker

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -9,20 +9,18 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *holder, *next;
 	unsigned long int index = 0;
 
-	if (!ht)
+	if ((*ht).size == 0 || !(*ht).array || !ht)
 		return;
 	for (; (*ht).size > index; index++)
 	{
 		if ((*ht).array[index] != NULL)
 		{
-			holder = (*ht).array[index];
-			while (holder != NULL)
+			for (holder = (*ht).array[index]; holder != NULL; holder = next)
 			{
 				next = (*holder).next;
 				free((*holder).key);
 				free((*holder).value);
 				free(holder);
-				holder = next;
 			}
 		}
 	}

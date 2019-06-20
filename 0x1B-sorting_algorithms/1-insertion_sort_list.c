72608b7010f34c0b1b8ff5f928425091f3c86f23
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-19 22:19:44 -0400

    nvm trying something new to be more efficient did not work

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -7,4 +7,37 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *here, *temp, *temp_n;
+	int check = 0;
+
+	if ((*list)->next == NULL || !list || *list == NULL)
+		return;
+	here = (*list)->next;
+	while (here != NULL)
+	{
+		temp = here;
+		while (temp->prev != NULL)
+		{
+			check = 0;
+			if (temp->n < temp->prev->n)
+			{
+				if (temp->prev->prev != NULL)
+					temp->prev->prev->next = temp;
+				temp->prev->next = temp->next;
+				temp_n = temp->prev->prev;
+				temp->prev->prev = temp;
+				if (temp->next != NULL)
+					temp->next->prev = temp->prev;
+				temp->next = temp->prev;
+				temp->prev = temp_n;
+				check = 1;
+				if (temp->prev == NULL)
+					*list = temp;
+				print_list(*list);
+			}
+			if (check == 0)
+				temp = temp->prev;
+		}
+		here = here->next;
+	}
 }

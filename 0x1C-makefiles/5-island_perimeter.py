3de72f698532e7503e3cf5f58ee297dc306f33bd
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-11 21:44:19 -0400

    test for perimeter finder + everything else

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
 """find the perimeter"""
 
-A = 3
-B = 5
 
 def island_perimeter(grid):
     """finds the perimeter using the find the perimeter function"""
     perimeter = 0
 
-    for x in range(0, A):
-        for y in range(0, B):
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
             if (grid[x][y]):
-                perimeter += (4 - neighborfinder(grid, x, y))
+                perimeter += 4
+                perimeter -= neighborfinder(grid, x, y)
 
     return perimeter
 
+
 def neighborfinder(grid, x, y):
     """finds the number of neighbors"""
-    count = 0;
+    count = 0
 
-    if (x > 0 and grid[x - 1][y]):
-        count += 1
-    if (y > 0 and grid[x][y - 1]):
-        count += 1
-    if (x < A - 1 and grid[x + 1][y]):
-        count += 1
-    if (y < B - 1 and grid[x][y + 1]):
-        count += 1
+    if (x > 0 and grid[x - 1][y] == 1):
+        count += 2
+    if (y > 0 and grid[x][y - 1] == 1):
+        count += 2
 
     return count

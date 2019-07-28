6b7f058fa6969a752431b69588a788822b21f00f
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-28 10:56:19 +0200

    adding to h file

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -35,6 +35,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_leaf(const binary_tree_t *node);
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 
 #endif /* binary_trees_h */

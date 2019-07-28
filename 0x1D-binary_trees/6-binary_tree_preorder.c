cf6980d7bb1790daf78dbe193439320a5da94831
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-28 10:52:54 +0200

    fixed the order of the travaersals

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -3,7 +3,7 @@
 /**
 * binary_tree_preorder - go through BT with preorder traversal
 * @tree: root of tree
-* @func: ptr to function to call
+* @func: pointer to function to call
 */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
@@ -12,6 +12,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
 	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
--- 0x1D-binary_trees/7-binary_tree_inorder.c
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -3,7 +3,7 @@
 /**
 * binary_tree_inorder - go through BT with inorder depthfirst search
 * @tree: root of tree
-* @func: function to call
+* @func: pointer of function to call
 */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
@@ -11,7 +11,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_inorder(tree->right, func);
-	func(tree->n);
 	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }

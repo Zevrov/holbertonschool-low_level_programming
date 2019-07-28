f4ce5556fa16241348f297431a2f6cc73fdcfda4
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-28 10:28:21 +0200

    update h file

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -11,4 +11,15 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+
+
+
 #endif

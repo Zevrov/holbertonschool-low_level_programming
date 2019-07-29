da2bc0af01ccaf6774ef917fbf5a430db75e02ef
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-29 11:46:20 +0200

    testing for last check on 17, set up 18 for testing

--- 0x1D-binary_trees/17-binary_tree_sibling.c
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -11,8 +11,19 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node->parent == NULL || node == NULL)
 		return (NULL);
 
-	if (node->parent->left == node)
-		return (node->parent->right);
+	if (node == node->parent->left)
+	{
+		if (node->parent->left != NULL)
+			return (node->parent->left);
+		else
+			return (NULL);
+	}
 
-	return (node->parent->left);
+	else
+	{
+		if (node->parent->right != NULL)
+			return (node->parent->right);
+		else
+			return (NULL);
+	}
 }
---
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -0,0 +1,8 @@
+#include "binary_trees.h"
+
+
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	
+}

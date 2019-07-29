8997e1bcedb547baba45559f370c69dcc5844c13
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-29 13:44:13 +0200

    finished 100

---
+++ 0x1D-binary_trees/100-binary_trees_ancestor.c
@@ -0,0 +1,67 @@
+#include "binary_trees.h"
+
+/**
+* binary_trees_ancestor - find the lowest common ancestor of two tree nodes
+* @first: first node
+* @second: second node
+* Return: common ancestor or NULL if there is none
+*/
+
+binary_tree_t *binary_trees_ancestor(binary_tree_t const *first,
+									 binary_tree_t const *second)
+{
+	binary_tree_t const *p1, *p2;
+	size_t d1, d2;
+
+	if (second == NULL || first == NULL)
+		return (NULL);
+
+	d2 = binary_tree_depth(second);
+	d1 = binary_tree_depth(first);
+
+	p1 = first;
+	p2 = second;
+
+	while (d2 < d1)
+	{
+		p1 = p1->parent;
+		d1--;
+	}
+
+	while (d2 > d1)
+	{
+		p2 = p2->parent;
+		d2--;
+	}
+
+	while (p2 != NULL && p1 != NULL)
+	{
+		if (p2 == p1)
+			return ((binary_tree_t *)p1);
+
+		p2 = p2->parent;
+		p1 = p1->parent;
+	}
+
+	return (NULL);
+}
+
+/**
+* binary_tree_depth - find depth of given node
+* @tree: le node
+* Return: depth of node, 0 on fail
+*/
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	binary_tree_t *old;
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	for (old = tree->parent; old != NULL; old = old->parent)
+		count++;
+
+	return (count);
+}
--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -48,5 +48,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(binary_tree_t const *first,
+									 binary_tree_t const *second);
 
 #endif /* binary_trees_h */

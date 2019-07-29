daef57396b3b523ce5d6e6627807892c9abd77a0
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-29 15:46:51 +0200

    flipped the left/right to make it rotate right instead of left

---
+++ 0x1D-binary_trees/104-binary_tree_rotate_right.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_rotate_left - sliiide to the left
+* @tree: the tree
+* Return: new tree
+*/
+
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *groot;
+
+	if (tree == NULL)
+		return (NULL);
+
+	groot = tree->left;
+
+	if (groot != NULL)
+	{
+		tree->left = groot->right;
+
+		if (groot->right != NULL)
+			groot->right->parent = tree;
+
+		groot->right = tree;
+
+		if (tree->parent != NULL)
+		{
+			if (tree->parent->left != tree)
+				tree->parent->right = groot;
+			else
+				tree->parent->left = groot;
+		}
+
+		groot->parent = tree->parent;
+		tree->parent = groot;
+	}
+
+	return (groot);
+}

d7992ef951bd8d5f5a0b1c256da30ce599455e8b
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-29 16:58:51 +0200

    ok doesnt work finding new way

--- 0x1D-binary_trees/110-binary_tree_is_bst.c
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -8,39 +8,3 @@
 */
 
 int binary_tree_is_bst(binary_tree_t const *tree)
-{
-	char side = 'x';
-
-	if (tree == NULL)
-		return (0);
-	if (tree->parent != NULL)
-	{
-		if (tree->parent->left == tree)
-			side = 'l';
-		else
-			side = 'r';
-	}
-	if (tree->left != NULL)
-	{
-		if (tree->left->n >= tree->n)
-			return (0);
-		if (side == 'l' && tree->left->n >= tree->parent->n)
-			return (0);
-		if (side == 'r' && tree->left->n <= tree->parent->n)
-			return (0);
-		if (binary_tree_is_bst(tree->left) == 0)
-			return (0);
-	}
-	if (tree->right != NULL)
-	{
-		if (tree->right->n <= tree->n)
-			return (0);
-		if (side == 'l' && tree->right->n >= tree->parent->n)
-			return (0);
-		if (side == 'r' && tree->right->n <= tree->parent->n)
-			return (0);
-		if (binary_tree_is_bst(tree->right) == 0)
-			return (0);
-	}
-	return (1);
-}

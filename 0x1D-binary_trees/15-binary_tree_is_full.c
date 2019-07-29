7045b4b712891909c4d1103d9471833da676a767
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-29 10:59:24 +0200

    testing 15

---
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_full - checks if BTree full
+* @tree: root node
+* Return: 1 = true, 0 = false
+*/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right != NULL)
+		return (0);
+
+	if (tree->left != NULL && tree->right == NULL)
+		return (0);
+
+	if (tree->right != NULL &&
+		binary_tree_is_full(tree->right) == 0)
+		return (0);
+
+	if (tree->left != NULL &&
+		binary_tree_is_full(tree->left) == 0)
+		return (0);
+
+	return (1);
+}

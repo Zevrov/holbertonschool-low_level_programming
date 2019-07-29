df238cf505c85093b4ba7fef9dc7f8689af0efaf
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-29 15:20:01 +0200

    testing solution vs checker

---
+++ 0x1D-binary_trees/114-bst_remove.c
@@ -0,0 +1,77 @@
+#include "binary_trees.h"
+
+/**
+* bst_remove - remove a node from a bst
+* @root: the tree
+* @value: value of node to remove
+* Return: root of the tree
+*/
+
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *plpl = NULL;
+	bst_t *leaf;
+
+	leaf = bst_search(root, value);
+	if (leaf == NULL)
+		return (root);
+	if (leaf->left != NULL && leaf->right == NULL)
+	{
+		for (plpl = leaf->left; plpl->right != NULL; plpl = plpl->right)
+			;
+		if (plpl != leaf->left)
+		{
+			plpl->parent->right = plpl->left;
+			plpl->left = leaf->left;
+		}
+		plpl->parent = leaf->parent, plpl->right = leaf->right;
+	}
+	else if (leaf->right != NULL)
+	{
+		for (plpl = leaf->right; plpl->left != NULL; plpl = plpl->left)
+			;
+		if (plpl != leaf->right)
+		{
+			plpl->parent->left = plpl->right;
+			plpl->right = leaf->right;
+		}
+		plpl->parent = leaf->parent, plpl->left = leaf->left;
+	}
+	if (leaf->parent != NULL && leaf->parent->right == leaf)
+		leaf->parent->right = plpl;
+	if (leaf->parent != NULL && leaf->parent->left == leaf)
+		leaf->parent->left = plpl;
+	free(leaf);
+	if (plpl != NULL && plpl->right != NULL)
+		plpl->right->parent = plpl;
+	if (plpl != NULL && plpl->left != NULL)
+		plpl->left->parent = plpl;
+	if (plpl->parent == NULL)
+		return (plpl);
+	return (root);
+}
+
+/**
+* bst_search - search for values in a bst
+* @tree: the tree
+* @value: the search
+* Return: node containing the search or NULL
+*/
+
+bst_t *bst_search(bst_t const *tree, int value)
+{
+	bst_t const *leaf;
+
+	leaf = tree;
+
+	while (leaf != NULL && leaf->n != value)
+	{
+		if (value > leaf->n)
+			leaf = leaf->right;
+
+		else if (value < leaf->n)
+			leaf = leaf->left;
+	}
+
+	return ((bst_t *)leaf);
+}
--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -59,5 +59,6 @@ int binary_tree_is_bst(binary_tree_t const *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(bst_t const *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
 
 #endif /* binary_trees_h */

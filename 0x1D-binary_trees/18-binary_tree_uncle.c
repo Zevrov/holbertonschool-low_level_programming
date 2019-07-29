3e1965267e72dd765a16612c19c0a9ae5030d0b8
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-29 12:05:14 +0200

    testing better solution for 18

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -8,13 +8,11 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node->parent->parent == NULL || node->parent == NULL
-		|| node == NULL)
+	if (node->parent->parent == NULL || node->parent == NULL || node == NULL)
 		return (NULL);
 
-	if (node->parent->parent->left !=
-		node->parent)
-		return (node->parent->parent->left);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
 
-	return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }

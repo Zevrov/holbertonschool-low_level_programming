2135e91318ae9cbacb2dd21a78f6dc809124929e
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-29 21:14:53 +0200

    fixed formatting

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node->parent->parent == NULL || node->parent == NULL || node == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
 	if (node->parent->parent->right == node->parent)

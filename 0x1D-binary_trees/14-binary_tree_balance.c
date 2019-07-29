b83d84fc81edd0794d733355fc080e6d912643a1
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-29 10:51:49 +0200

    typo

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -28,8 +28,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (size_comparer(binary_tree_height(tree->right),
-			binary_tree_height(tree->left)) + 1);
+	return (size_comparer(binary_tree_height(tree->left),
+			binary_tree_height(tree->right)) + 1);
 }
 
 /**

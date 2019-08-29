6b2b6990432d9a4fbcac78c4364ace861d548d67
Wolf Hopkins <719@holbertonschool.com>
2019-08-29 13:56:20 -0400

    found an extra semicolon on line 15

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	int i = 0;
 
-	while ((size_t) i < size;)
+	while ((size_t) i < size)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)

3f8ff48573cb1e3e0cd6501b9d6b834a55488862
Wolf Hopkins <719@holbertonschool.com>
2019-08-29 13:54:11 -0400

    fixed program to follow Betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,14 +8,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    int i = 0;
+	int i = 0;
 
-    while ((size_t) i < size;)
-    {
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-        i++;
-    }
-    return (-1);
+	while ((size_t) i < size;)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
 }

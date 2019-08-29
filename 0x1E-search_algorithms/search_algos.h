ef8b74a99da8ee42c6e300d911847286e850fc00
Wolf Hopkins <719@holbertonschool.com>
2019-08-29 13:37:16 -0400

    updating hfile with our search functions

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef ALGOS_H
+#define ALGOS_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* ALGOS_H */
